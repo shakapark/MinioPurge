eb9533875285464ef152cd5f7d6de4e04787fd8c
Shakapark <shakapark1994@gmail.com>
2018-04-10 16:52:21 +0200

    Try To Fix TAB1

--- entrypoint.sh
+++ entrypoint.sh
@@ -23,9 +23,8 @@ function purge() {
 }
 
 
-TAB1=$(mc --json ls $SRC | grep -Eo '"key":.*?[^\\]",'|awk -F':' '{print $2}' | cut -d \" -f2 | cut -d / -f1 | tr " " "\n")
+TAB1=$(mc --json ls $SRC | grep -Eo '"key":.*?[^\\]",'|awk -F':' '{print $2}' | cut -d \" -f2 | cut -d / -f1 | tr " "\n")
 echo $TAB1
 TAB2=($(echo $BUCKETS | tr ',' "\n"))
 echo $TAB2
-sleep 10
-purge
+#purge
