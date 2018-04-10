c3a9864ccedae8cc87e5c5e640e19afde2f1294c
Shakapark <shakapark1994@gmail.com>
2018-04-10 14:15:21 +0200

    Fix Environement Variable

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,5 @@
 FROM minio/mc:RELEASE.2018-02-09T23-07-36Z
 
-ENV MINIO=minio_needed_to_be_purge
-# List of bucket separate by ; all buckets if empty
-ENV BUCKETS
 # Number of retention days
 ENV RETENTION=7
 
--- entrypoint.sh
+++ entrypoint.sh
@@ -4,14 +4,14 @@ function purge() {
   echo "Starting Purge"
 
   if ["$BUCKETS" == ""]; then
-    $TAB=$(mc --json ls $MINIO | grep -Eo '"key":.*?[^\\]",'|awk -F':' '{print $2}' | cut -d \" -f2 | cut -d / -f1 | tr " " "\n")
+    $TAB=$(mc --json ls $SRC | grep -Eo '"key":.*?[^\\]",'|awk -F':' '{print $2}' | cut -d \" -f2 | cut -d / -f1 | tr " " "\n")
   else
     IFS=';' read -ra $TAB <<< "$BUCKETS"
   fi
 
 #  for BUCKET in "${TAB[@]}"
 #  do
-#    mc rm --recursive --force --older-than=$RETENTION $MINIO/$BUCKET/*
+#    mc rm --recursive --force --older-than=$RETENTION $SRC/$BUCKET/*
 #  done
 
   echo "Purge Done"
