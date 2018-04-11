84d4c8b4bba5a3da284438642596101397ed0575
Shakapark <shakapark1994@gmail.com>
2018-04-11 11:19:40 +0200

    Fix Last Commit

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
-l#!/bin/bash
+#!/bin/bash
 
 function purge() {
   echo "Starting Purge"
@@ -14,7 +14,7 @@ function purge() {
   for BUCKET in "${TAB[@]}"
   do
     echo $BUCKET
-    FILES=$(mc ls $SRC/$BUCKET)
+    FILES=$(mc ls $SRC/$BUCKET/)
     for FILE in $FILES
     do
       echo $FILE
